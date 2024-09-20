b6e4a0a50e0c01642a6413149a74e03fb1297a1d
e-dinkov <emopro1000@gmail.com>
2024-09-20 21:37:26 +0300

    New update

---
+++ Data Types and Variables/ascii.js
@@ -0,0 +1,9 @@
+function fundamentals(letter1, letter2, letter3) {
+    console.log(letter3+letter2+letter1)
+    console.log(letter3.charCodeAt() + ' ' + letter2.charCodeAt() + ' '  + letter1.charCodeAt())
+  
+    
+    
+  
+  }
+  fundamentals('a','b','c')
---
+++ Data Types and Variables/poundsToDollars.js
@@ -0,0 +1,9 @@
+function fundamentals(pounds) {
+    // •	1 British Pound = 1.31 Dollars
+    let dollars = pounds * 1.31
+    console.log(dollars.toFixed(3))
+
+
+
+}
+fundamentals(80)
